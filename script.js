bd8c03c46da5fdb74d4336d2b74393930038b976
GrandScalleywag <paulinojayden9@gmail.com>
2024-08-07 16:50:56 +0000

    new

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function verifyAge (age) {
     alert('You are a senior')
   }
 }
+
+
+
+function uploadAndIdentifyPlant() {
+  // get photo from the frontend
+  var photoInput = 
+  document.getElementById('photoInput');
+  console.log(photoInput)
+  console.log(photoInput.files[0])
+  // if no photo was selected and the user clicks submit
+  // let the user know they need to upload a photo first
+  if (photoInput.files.length === 0) {
+    alert('Please select a photo to upload');
+    return;
+  }
+
+  // select the first file from the files folder/array from the upload button
+  var selectedFile = photoInput[0];
+}
